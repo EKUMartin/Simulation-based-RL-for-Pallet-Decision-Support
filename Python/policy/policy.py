2faf613be849546b63a5e46ab0d938de65a419e0
movieunknown85@gmail.com <movieunknown85@gmail.com>
2026-04-26 16:38:49 +0900

    신경망수정

--- Python/policy/policy.py
+++ Python/policy/policy.py
@@ -12,6 +12,10 @@ class ActorCritic(nn.Module):
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1), 
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1), 
             nn.ReLU()
         )
         self.box_info_dim = 9
